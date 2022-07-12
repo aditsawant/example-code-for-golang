222790b93b04a3fc3b3b72cc1f24d4a0e9595002
Adit Sawant <aditsawant@gmail.com>
2022-07-12 12:27:03 +0530

    http-get

---
+++ http-req/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Getting Content-Type value from the HTTP response")
+
+	url := "http://www.linkedin.com"
+
+	ctype, err := getCType(url)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	} else {
+		fmt.Println(ctype)
+	}
+}
+
+func getCType(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	ctype := resp.Header.Get("Content-Type")
+	if ctype == "" {
+		return "", fmt.Errorf("Can't find Content-Type in the Header")
+	}
+
+	return ctype, nil
+}
